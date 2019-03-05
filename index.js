dae64875104029b341951acc1732bc9b6dbbf91f
Reed Stewart <reedostewart@gmail.com>
2019-03-06 12:41:18 +1300

    Fixed bug: msg was only being changed when water was the last string in a sentence

--- index.js
+++ index.js
@@ -13,24 +13,50 @@ var watermelonArr = [
 
 client.on('message', (msg) => {
     try {
+        if (msg.content === '!melonlist') {
+            const embed = new Discord.RichEmbed()
+                .setTitle('water-melon list!')
+                .addField('water    =>      melon', '.', true)
+                .addField('Water    =>      Melon', '.', false);
+            msg.channel.send(embed);
+        }
+
         if (!msg.author.bot) {
             var msgArray = msg.content.split(' ');
+            //comparison array to check if message needs to be reposted
+            var checkMsgArray = msg.content.split(' ');
 
             for (x = 0; x < msgArray.length; x++) {
-                for(i = 0; i < watermelonArr.length; i++){
+                for (i = 0; i < watermelonArr.length; i++) {
                     if (msgArray[x] == watermelonArr[i][0]) {
-                        msgArray[x] = watermelonArr[i][1];
-                        if(x == msgArray.length - 1){
-                            msg.delete();
-                            msg.channel.send(`${msg.author.username}: ` + msgArray.join(' '));
-                        }
+                        msgArray[x] = watermelonArr[i][1];                        
                     }
-                }                
+                }
+                if (x == msgArray.length - 1) {
+                    if (arraysEqual(msgArray, checkMsgArray))
+                        break;
+                    else {
+                        msg.delete();
+                        msg.channel.send(`${msg.author.username}: ` + msgArray.join(' '));
+                    }
+                }
             }
+
         }
     }
     catch (err) {
         console.log(err)
+    }
+});
+
+client.on('message', (msg) => {
+    try {
+        if (msg.content === '!melonlist') {
+
+        }
+    }
+    catch (err) {
+        console.log(err);
     }
 });
 
@@ -39,3 +65,14 @@ client.on('ready', () => {
 });
 
 client.login(token);
+
+
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length)
+        return false;
+    for (var i = arr1.length; i--;) {
+        if (arr1[i] !== arr2[i])
+            return false;
+    }
+    return true;
+}
