32593a83cb8cda2cdcfff438135b691de554c2c2
RedStewart <reedostewart@gmail.com>
2021-03-23 15:37:16 +1300

    Big ol refactor

--- index.js
+++ index.js
@@ -1,78 +1,57 @@
 const Discord = require('discord.js');
-const path = require('path');
-const config = require(path.join(__dirname, 'config.json'));
-
-const token = config.token;
-
+const ora = require('ora');
 const client = new Discord.Client();
+require('dotenv').config();
 
-var watermelonArr = [
-    ['water', 'melon'],
-    ['Water', 'Melon']
-];
+const REGEX = new RegExp('water', 'i');
 
 client.on('message', (msg) => {
-    try {
-        if (msg.content === '!melonlist') {
-            const embed = new Discord.RichEmbed()
-                .setTitle('water-melon list!')
-                .addField('water    =>      melon', '.', true)
-                .addField('Water    =>      Melon', '.', false);
-            msg.channel.send(embed);
+  try {
+    if (!msg.author.bot) {
+      let msgArr = msg.content.split(' ');
+
+      if (!checkWaterStringExists(msgArr)) return;
+
+      msgArr.forEach((el, index) => {
+        if (REGEX.test(el)) {
+          msgArr[index] = buildWaterString(el);
         }
+      });
 
-        if (!msg.author.bot) {
-            var msgArray = msg.content.split(' ');
-            //comparison array to check if message needs to be reposted
-            var checkMsgArray = msg.content.split(' ');
-
-            for (x = 0; x < msgArray.length; x++) {
-                for (i = 0; i < watermelonArr.length; i++) {
-                    if (msgArray[x] == watermelonArr[i][0]) {
-                        msgArray[x] = watermelonArr[i][1];                        
-                    }
-                }
-                if (x == msgArray.length - 1) {
-                    if (arraysEqual(msgArray, checkMsgArray))
-                        break;
-                    else {
-                        msg.delete();
-                        msg.channel.send(`${msg.author.username}: ` + msgArray.join(' '));
-                    }
-                }
-            }
-
-        }
+      sendDiscordMessage(msg, msgArr);
     }
-    catch (err) {
-        console.log(err)
-    }
-});
-
-client.on('message', (msg) => {
-    try {
-        if (msg.content === '!melonlist') {
-
-        }
-    }
-    catch (err) {
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 client.on('ready', () => {
-    console.log('Bot online...');
+  ora('Bot Online...').start();
 });
 
-client.login(token);
+client.login(process.env.DISCORDTOKEN);
 
+const checkWaterStringExists = (arr) => {
+  let waterFlag = false;
+  arr.forEach((el) => {
+    if (REGEX.test(el)) waterFlag = true;
+  });
+  return waterFlag;
+};
 
-function arraysEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length)
-        return false;
-    for (var i = arr1.length; i--;) {
-        if (arr1[i] !== arr2[i])
-            return false;
-    }
-    return true;
-}
+const buildWaterString = (el) => {
+  let melonString = ['m', 'e', 'l', 'o', 'n'],
+    charArr = [...el];
+
+  for (let x = 0; x < charArr.length; x++) {
+    if (charArr[x] === charArr[x].toUpperCase())
+      melonString[x] = melonString[x].toUpperCase();
+  }
+
+  return melonString.join('');
+};
+
+const sendDiscordMessage = (msg, msgArr) => {
+  msg.delete();
+  msg.channel.send(`**${msg.author.username}:** ${msgArr.join(' ')}`);
+};
